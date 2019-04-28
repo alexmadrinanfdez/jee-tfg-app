1502c49f55432e59ee26be16d47e442c8cccb643
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2019-04-28 14:06:13 +0200

    vista inicial & login

---
+++ src/es/upm/dit/isst/webLab/dao/ProfessorDAOImplementation.java
@@ -0,0 +1,88 @@
+package es.upm.dit.isst.webLab.dao;
+
+import java.util.Collection;
+
+import org.hibernate.Session;
+
+import es.upm.dit.isst.webLab.model.Professor;
+
+public class ProfessorDAOImplementation implements ProfessorDAO {
+	private static ProfessorDAOImplementation instance = null;
+	private ProfessorDAOImplementation() {}
+	public static ProfessorDAOImplementation getInstance() {
+		if( null == instance )
+			instance = new ProfessorDAOImplementation();
+		return instance;
+	}
+	@Override
+	public void create(Professor professor) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.save( professor );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// manejar excepciones
+		} finally {
+			session.close();
+		}
+		
+	}
+	@Override
+	public Professor read(String email) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			Professor professor = session.get( Professor.class, email );
+			session.getTransaction().commit();
+			return professor;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	@Override
+	public void update(Professor professor) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate( professor );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// manejar excepciones
+		} finally {
+			session.close();
+		}
+		
+	}
+	@Override
+	public void delete(Professor professor) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.delete( professor );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// manejar excepciones
+		} finally {
+			session.close();
+		}
+		
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<Professor> readAll() {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			Collection<Professor> professors = session.createQuery( "from Professor" ).list();
+			session.getTransaction().commit();
+			return professors;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+}
---
+++ src/es/upm/dit/isst/webLab/dao/TFGDAOImplementation.java
@@ -0,0 +1,87 @@
+package es.upm.dit.isst.webLab.dao;
+
+import java.util.Collection;
+
+import org.hibernate.Session;
+
+import es.upm.dit.isst.webLab.model.TFG;
+
+public class TFGDAOImplementation implements TFGDAO {
+	private static TFGDAOImplementation instance = null;
+	private TFGDAOImplementation() {}
+	public static TFGDAOImplementation getInstance() {
+		if( null == instance ) 
+			instance = new TFGDAOImplementation();
+		return instance;
+	}
+	@Override
+	public void create(TFG tfg) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.save( tfg );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// manejar excepciones
+		} finally {
+			session.close();
+		}
+		
+	}
+	@Override
+	public TFG read(String email) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			TFG tfg = session.get( TFG.class, email );
+			session.getTransaction().commit();
+			return tfg;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	@Override
+	public void update(TFG tfg) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate( tfg );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// manejar excepciones
+		} finally {
+			session.close();
+		}		
+	}
+	@Override
+	public void delete(TFG tfg) {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.delete( tfg );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// manejar excepciones
+		} finally {
+			session.close();
+		}
+		
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<TFG> readAll() {
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			Collection<TFG> tfgs = session.createQuery( "from TFG" ).list();
+			session.getTransaction().commit();
+			return tfgs;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+}
